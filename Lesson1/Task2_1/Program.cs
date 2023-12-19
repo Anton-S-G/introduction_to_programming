b8ae1d15ff80bfe2399e4b2ffca96a977ad57882
Anton Sopov <recorrd@yandex.ru>
2023-12-19 23:17:11 +0300

    add new tasks

---
+++ Lesson1/Task2_1/Program.cs
@@ -0,0 +1,4 @@
+﻿int a = 15, c = 21, d = 51;
+double b = 8.0;
+double check = a * b / (c + d);
+Console.WriteLine(check);
---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 50, b = 34, c = 5, d = 98, e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
