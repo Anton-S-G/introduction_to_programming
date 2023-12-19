383bfd7e9040558aaa9adff94e0d88c59d88a457
Anton Sopov <recorrd@yandex.ru>
2023-12-19 22:39:03 +0300

    add some variables lesson:task1

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine(value);
+
+Double d = 5.55;
+char c ='1';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
